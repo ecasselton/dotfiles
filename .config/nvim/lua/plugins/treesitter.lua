d75b3c42f9a2ba193d5ffe1aecccc2bbbe0e325f
ecasselton <elliotcelliot@gmail.com>
2024-11-17 18:41:21 +0000

    nvim: added treesitter context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,13 +6,14 @@ return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
         'nvim-treesitter/nvim-treesitter-textobjects',
+        'nvim-treesitter/nvim-treesitter-context',
     },
     build = ':TSUpdate',
 
     config = vim.defer_fn(function()
         require('nvim-treesitter.configs').setup {
             -- Add languages to be installed here that you want installed for treesitter
-            ensure_installed = { 'c', 'c_sharp', 'lua', 'python', 'bash' },--, 'markdown', 'markdown_inline' },
+            ensure_installed = { 'c', 'c_sharp', 'lua', 'python', 'bash' },--, 'markdown', 'markdown_inline' ,
 
             -- Add languages to be ignored here (when ensure_installed = all)
             ignore_install = { "latex" },
@@ -71,16 +72,30 @@ return {
                         ['[]'] = '@class.outer',
                     },
                 },
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        ['<leader>a'] = '@parameter.inner',
-                    },
-                    swap_previous = {
-                        ['<leader>A'] = '@parameter.inner',
-                    },
-                },
+                -- swap = {
+                --     enable = true,
+                --     swap_next = {
+                --         ['<leader>a'] = '@parameter.inner',
+                --     },
+                --     swap_previous = {
+                --         ['<leader>A'] = '@parameter.inner',
+                --     },
+                -- },
             },
+        }
+        require'treesitter-context'.setup{
+            enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+            max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+            min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+            line_numbers = true,
+            multiline_threshold = 20, -- Maximum number of lines to show for a single context
+            trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+            mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+            -- Separator between context and content. Should be a single character string, like '-'.
+            -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+            separator = nil,
+            zindex = 20, -- The Z-index of the context window
+            on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
         }
     end, 0)
 }
