112d03bb784ef3ffe751b1941fab7113a43fef80
ecasselton <elliotcelliot@gmail.com>
2024-11-17 18:28:38 +0000

    nvim: vimtex config now specifies using zathura and compile using bibtex

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -3,20 +3,11 @@ return {
 	lazy = false,
 
 	config = function()
-		vim.g.vimtex_imaps_leader = '<C-l>'
 		vim.g.vimtex_mappings_prefix = '<localleader>'
 		vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
-		-- vim.g.vimtex_compiler_tectonic = 
+		vim.g.vimtex_view_method = 'zathura_simple'
 
-		-- [[ Me trying to disable automatic compilation,
-		--    but maybe it's not so bad ]]
-		--
-		-- local compile = function()
-		-- 	vim.cmd('silent !latexmk')
-		-- 	print('Document compiled')
-		-- end
-		-- vim.keymap.set('n', '\\l', compile, { silent = true, desc = '[L]atex compile' })
-		-- vim.g.vimtex_latexmk_continuous = 0
-
+		vim.g.Tex_DefaultTargetFormat = 'pdf'
+		vim.g.Tex_MultipleCompileFormats ='pdf,bibtex,pdf'
 	end
 }
