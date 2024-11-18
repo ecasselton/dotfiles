5808a6a1085cd891be840322d8812e8f92aad8ca
ecasselton <elliotcelliot@gmail.com>
2024-11-18 16:41:29 +0000

    nvim: removed unneded luasnip config bits

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -28,24 +28,7 @@ return {
 			end
 		end, {silent = true})
 
-		local i = ls.insert_node
-		local s = ls.snippet
-		local t = ls.text_node
-		local sn = ls.snippet_node
-		local fmt = require("luasnip.extras.fmt").fmt
-		local extras = require("luasnip.extras")
-		local rep = extras.rep
-
 
 		require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/lua/snippets/"})
-		-- ls.add_snippets("tex", {
-		-- 	-- trigger is `fn`, second argument to snippet-constructor are the nodes to insert into the buffer on expansion.
-		-- 	s({name = "begin{env}", trig = "\\begin", snippetType = "autosnippet"},
-		-- 		fmt([[
-		-- 	\begin{<>}
-		-- 	
-		-- 	\end{<>}
-		-- 	]], { i(1), rep(1) }, { delimiters = "<>" }))
-		-- })
 	end
 }
