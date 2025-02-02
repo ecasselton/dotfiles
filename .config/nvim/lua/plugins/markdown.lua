2cea3e4a7bf0fe0d67c8abc395436475ae9d8964
ecasselton <elliotcelliot@gmail.com>
2025-02-02 22:34:39 +0000

    nvim: switched markdown plugin to render-markdown.nvim

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,43 @@
 return {
-	'ixru/nvim-markdown',
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    config = function()
+		local markdown = require("render-markdown")
+		vim.keymap.set("n", "<leader>mt", markdown.toggle, { desc = "Render [M]arkdown [T]oggle" })
 
-	config = function ()
-		vim.g.vim_markdown_conceal = 0
-		vim.g.vim_markdown_folding_disabled = 1
-	end
+		vim.treesitter.language.register('markdown', 'vimwiki')
+
+		markdown.setup({
+			file_types = { 'markdown', 'vimwiki' },
+
+			bullet = {
+				icons = { '' },
+				-- foregrounds = {}
+			},
+
+			heading = {
+				position = 'right',
+				backgrounds = {
+					'transparent',
+					'transparent',
+					'transparent',
+					'transparent',
+					'transparent',
+					'transparent',
+					'transparent',
+				},
+				foregrounds = {
+					'RenderMarkdownH1',
+					'RenderMarkdownH2',
+					'RenderMarkdownH3',
+					'RenderMarkdownH4',
+					'RenderMarkdownH5',
+					'RenderMarkdownH6',
+				},
+			}
+		})
+	end,
 }
+
