990e11dd4d68393df59069ae61baac44f085e01b
ecasselton <elliotcelliot@gmail.com>
2025-02-06 15:04:34 +0000

    nvim vimtex: update config to put aux files in subdir

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -6,6 +6,9 @@ return {
 		vim.g.vimtex_mappings_prefix = '<localleader>'
 		vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
 		vim.g.vimtex_view_method = 'zathura_simple'
+		vim.g.vimtex_compiler_latexmk = {
+			aux_dir = "aux", -- create a directory called aux that will contain all the auxiliary files
+		}
 
 		vim.g.Tex_DefaultTargetFormat = 'pdf'
 		vim.g.Tex_MultipleCompileFormats ='pdf,bibtex,pdf'
